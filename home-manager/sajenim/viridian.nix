531b178421971973cc24c8d23ba87a7cb50acebb
jasmine <its.jassy@pm.me>
2025-06-06 18:34:15 +0800

    setup viridian with some programs

--- home-manager/sajenim/viridian.nix
+++ home-manager/sajenim/viridian.nix
@@ -1,5 +1,25 @@
-{...}: {
+{
+  inputs,
+  pkgs,
+  ...
+}: {
   imports = [
     ./global
   ];
+
+  home.packages = with pkgs;
+    [
+      weechat
+    ]
+    ++ [
+      inputs.nixvim.packages.${pkgs.system}.default
+    ];
+
+  programs.tmux = {
+    enable = true;
+    extraConfig = ''
+      set -g default-terminal "tmux-256color"
+      set -g status off
+    '';
+  };
 }
