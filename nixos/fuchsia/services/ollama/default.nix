34ed155ae3e02470bbb17b15ee5ea80e805bb8f3
jasmine <its.jassy@pm.me>
2025-07-23 08:06:28 +0800

    update available models

--- nixos/fuchsia/services/ollama/default.nix
+++ nixos/fuchsia/services/ollama/default.nix
@@ -10,13 +10,9 @@
 
     # Language models to install
     loadModels = [
-      "deepseek-coder-v2"
-      # these were dependencies for continue.dev
-      # but idk if i'll end up using it
-      # for now we shall keep them
       "llama3.1:8b"
-      "qwen2.5-coder:1.5b-base"
-      "nomic-embed-text:latest"
+      # Uncensored models
+      "huihui_ai/gemma3-abliterated:4b"
     ];
   };
 }
