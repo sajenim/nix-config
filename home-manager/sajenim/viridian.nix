2c286d65fb0b5885f33c4d89ba814d0ec0ef218b
jasmine <its.jassy@pm.me>
2025-10-08 10:21:16 +0800

    refactor(viridian): simplify home-manager config and enable tmux status
    Remove unused packages (weechat, nixvim) and enable tmux status bar for better session visibility on the server.

--- home-manager/sajenim/viridian.nix
+++ home-manager/sajenim/viridian.nix
@@ -1,25 +1,13 @@
-{
-  inputs,
-  pkgs,
-  ...
-}: {
+{...}: {
   imports = [
     ./global
   ];
-
-  home.packages = with pkgs;
-    [
-      weechat
-    ]
-    ++ [
-      inputs.nixvim.packages.${pkgs.system}.default
-    ];
 
   programs.tmux = {
     enable = true;
     extraConfig = ''
       set -g default-terminal "tmux-256color"
-      set -g status off
+      set -g status on
     '';
   };
 }
