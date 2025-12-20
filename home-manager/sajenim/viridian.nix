efbbfecb2c97b04e42249a32bda5d312df4a95a5
jasmine <its.jassy@pm.me>
2025-12-20 19:33:19 +0800

    feat(viridian): add CLI features and disable tmux status bar
    - Import CLI feature configuration (includes zsh setup)
    - Disable tmux status bar for cleaner interface

--- home-manager/sajenim/viridian.nix
+++ home-manager/sajenim/viridian.nix
@@ -1,13 +1,14 @@
 {...}: {
   imports = [
     ./global
+    ./features/cli
   ];
 
   programs.tmux = {
     enable = true;
     extraConfig = ''
       set -g default-terminal "tmux-256color"
-      set -g status on
+      set -g status off
     '';
   };
 }
