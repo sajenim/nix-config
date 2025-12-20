61e348a83ff2cf9af6b181d03a6ad50576289ee9
jasmine <its.jassy@pm.me>
2025-12-20 19:32:20 +0800

    feat(inspircd): configure proper logging directory
    - Ensure log directory exists via systemd LogsDirectory
    - Update log path to use absolute path /var/log/inspircd/ircd.log

--- nixos/viridian/services/inspircd/default.nix
+++ nixos/viridian/services/inspircd/default.nix
@@ -4,6 +4,9 @@
     config = builtins.readFile ./inspircd.conf;
   };
 
+  # Ensure log directory exists
+  systemd.services.inspircd.serviceConfig.LogsDirectory = "inspircd";
+
   environment.etc = {
     "inspircd/inspircd.motd".source = ./inspircd.motd;
   };
