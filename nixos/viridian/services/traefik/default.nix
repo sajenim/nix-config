f9189467239d5b6ee36cd009ae6276db5f71893a
jasmine <its.jassy@pm.me>
2026-01-31 14:29:27 +0800

    Revert "refactor(traefik): remove geoblock middleware in favor of crowdsec"
    This reverts commit c998c99bfcbfe071a765cad6f53f18d8f15c6673.

--- nixos/viridian/services/traefik/default.nix
+++ nixos/viridian/services/traefik/default.nix
@@ -70,7 +70,13 @@
 
       # Install plugins
       experimental.plugins = {
-        # Authorize or block requests from IPs based on their reputation and behaviour.
+        # Block or allow requests based on their country of origin.
+        geoblock = {
+          moduleName = "github.com/PascalMinder/geoblock";
+          version = "v0.2.7";
+        };
+
+        # Authorize or block requests from IPs based on there reputation and behaviour.
         bouncer = {
           moduleName = "github.com/maxlerebourg/crowdsec-bouncer-traefik-plugin";
           version = "v1.3.5";
@@ -95,6 +101,7 @@
 
           # Enable some middlewares on all routers that use this entrypoint
           http.middlewares = [
+            "geoblock@file"
             "crowdsec@file"
           ];
 
